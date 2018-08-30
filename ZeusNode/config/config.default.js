12e359cbdb16555b978d722714097746bc5fde3f
codeliling <maximuslee@126.com>
2018-08-30 17:01:17 +0800

    example

--- ZeusNode/config/config.default.js
+++ ZeusNode/config/config.default.js
@@ -9,5 +9,11 @@ module.exports = appInfo => {
   // add your config here
   config.middleware = [];
 
+  config.view = {
+    defaultViewEngine:'nunjucks',
+    mapping:{
+      '.tpl':'nunjucks',
+    },
+  };
   return config;
 };
